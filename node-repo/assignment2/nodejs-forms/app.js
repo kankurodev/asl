cb7ebb6dc3708dd51b79ca983a9058d708d76a55
Toby Horn <kankuro865@gmail.com>
2018-04-15 20:30:41 -0500

    Fixed error relating to navItems length being undefined. Added express-validator module and implemented it throughout the project. Added express-validator to package.json dependencies. Finalized and finished assignment.

--- node-repo/assignment2/nodejs-forms/app.js
+++ node-repo/assignment2/nodejs-forms/app.js
@@ -3,9 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser'); //require body parser
+var expressValidator = require('express-validator'); //require express validator
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -14,13 +15,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator()); //use express validator
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +38,25 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//Express Validator Middleware
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+      var namespace = param.split("."),
+          root = namespace.shift(),
+          formParam = root;
+
+      while(namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+      }
+
+      return {
+        param : formParam,
+          msg : msg,
+          value : value
+      };
+
+    }
+}));
 
 module.exports = app;
--- node-repo/assignment2/nodejs-forms/routes/index.js
+++ node-repo/assignment2/nodejs-forms/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// GET home page
 router.get('/', function(req, res, next) {
     res.render('index', {
         title: 'Home',
@@ -11,6 +11,8 @@ router.get('/', function(req, res, next) {
         ]});
 });
 
+
+// GET register page
 router.get('/register', function(req, res, next) {
     res.render('register', {
         title: 'Register',
@@ -20,15 +22,47 @@ router.get('/register', function(req, res, next) {
         ]});
 });
 
-
+// POST registered page
 router.post('/registered', function(req, res, next) {
-    res.render('index', {
-        title: 'Home',
-        message: "Registration Complete!",
-        navItems: [
-            {link:'/',content:'Home'},
-            {link:'/register',content:'Register'}
-        ]});
+
+    //create input field value variables
+    var username = req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+
+    //validate inputs aren't empty and passwords match
+    req.checkBody("username", "Username is required.").notEmpty();
+    req.checkBody("email", "Email is required.").notEmpty();
+    req.checkBody("email", "Please enter a valid Email.").isEmail();
+    req.checkBody("password", "Password is required.").notEmpty();
+    req.checkBody("password2", "Passwords do not match.").equals(req.body.password);
+
+    //capture errors in a variable
+    var errors = req.validationErrors();
+
+    //If errors exist, render the register view and pass in the errors
+    if (errors) {
+        res.render('register', {
+            title: 'Register',
+            errors: errors,
+            navItems: [
+                {link: '/', content: 'Home'},
+                {link: '/register', content: 'Register'}
+            ]
+        });
+    } else {
+
+        //If no errors, then render the home page and pass in the success message
+        res.render('index', {
+            title: 'Home',
+            message: "Registration Complete!",
+            navItems: [
+                {link: '/', content: 'Home'},
+                {link: '/register', content: 'Register'}
+            ]
+        });
+    }
 });
 
 module.exports = router;
